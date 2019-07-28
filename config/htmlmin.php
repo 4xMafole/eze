435055210836943aaf34b64cb864e335263ceacf
4cruxt Unknown <4cruxt.unknown@gmail.com>
2019-07-28 16:18:57 -0700

    ignore mail during minification

--- config/htmlmin.php
+++ config/htmlmin.php
@@ -61,7 +61,7 @@ return [
         'resources/views/html',
         'resources/views/notifications',
         'resources/views/markdown',
-        'resources/views/vendor/emails',
+        'resources/views/vendor/mail',
         'resources/views/vendor/html',
         'resources/views/vendor/notifications',
         'resources/views/vendor/markdown',
