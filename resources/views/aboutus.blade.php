898caa0b9edf83d68adc550534236830659fe274
4cruxt Unknown <4cruxt.unknown@gmail.com>
2019-10-07 23:06:35 -0700

    Updated about us page.

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -1,8 +1,13 @@
 <div class="scollable">
 	<div class="about_temp">
-	    <h2>About</h2>
+	    <h2><b>About</b></h2>
+	    <hr>
+	    <br>
+	    <h4>Vision</h4>
+	    <p><b>"Quality over quantity"</b></p>
+	    <h4>Founder</h4>
 	    <h4><b>Erick Mafole (CEO, Founder)</b></h4>
-	    <p><b>Erick Mafole</b> is the founder of Whyeze, a place where community members challenge to their best. He is accountable for all company's activities and strategy.</p>
-	    <p><b>Furthermore,</b> Mafole focuses on how to improve social interactions through competition. Hence motivates self-assertive, making every community member to participate in one way or another.</p>  
+	    <p><b>Erick Mafole</b> is the founder of Whyeze, a place where community members discover and approve quality products and services. He is accountable for all company's activities and strategy.</p>
+	    <p><b>Furthermore,</b> Mafole focuses on how to improve products and services of all kinds through competition. Hence motivates assertiveness, making every community member to participate in one way or another.</p>  
 	</div>
 </div>
